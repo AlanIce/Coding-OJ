e99dc9a170619e790e5bee2a98bd69ebb0fe2ac0
Alan-Windows <2198928117@qq.com>
2017-07-17 21:25:21 +0800

    complete LeetCode item 11-15

---
+++ Java/LeetCode/100/item015.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] > sum) hi--;
+                else if (nums[lo] + nums[hi] < sum) lo++;
+                else {
+                    res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                    lo++; hi--;
+                }
+            }
+        }
+        return res;
+    }
+}
