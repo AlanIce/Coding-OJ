8118da341cea1182f1adb9bd83a4a504fec1700c
Alan-Windows <2198928117@qq.com>
2017-08-07 21:26:57 +0800

    update items 061-065

---
+++ Java/LeetCode/100/075/item062.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m > n) return uniquePaths(n, m);
+        long count = 1;
+        for (int i = n; i < m + n - 1; i++) count *= i;
+        for (int i = 2; i < m; i++) count /= i;
+        return (int) count;
+    }
+}
